f0f7689adde8a1573264a519f017fb402ffedb9e
winstondant007 <winstondant@mail.ru>
2021-08-29 15:37:32 +0300

    first site of my portfolio

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+    
+    testWebP(function (support) {
+    
+    if (support == true) {
+    document.querySelector('body').classList.add('webp');
+    }else{
+    document.querySelector('body').classList.add('no-webp');
+    }
+    });
+
+const langItems = document.querySelector('.lang__items');
+langItems.addEventListener('click', (event) => {
+	
+	Array.from(langItems.children).forEach(child => {
+		child.classList.remove('lang__item_active');
+	});
+	event.target.classList.add('lang__item_active');
+});
